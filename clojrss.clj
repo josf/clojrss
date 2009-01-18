8d2bf2706afa74544049ae7e69caf5f13df5ae92
Joseph Fahey <joseph@feather.eerayell.net>
2009-01-18 18:36:15 +0100

    Working on http stuff. Problem with 304s.

--- clojrss.clj
+++ clojrss.clj
@@ -1,6 +1,12 @@
-(ns net.ramure.clojrss
-  (:import (java.io.BufferedReader FileReader)))
+;(ns net.ramure.clojrss
+;  (:import (java.io.BufferedReader FileReader)))
 
+(import '(java.net URL)
+        '(java.lang StringBuilder)
+        '(java.io BufferedReader InputStreamReader))
+
+(import '(clj-http-client.core))
+(use '[clojure.contrib.duck-streams :only (reader)])
 (defstruct feed :name :title :url :type :etag :last-update)
 
 (defn init-feed-db [] 
@@ -25,5 +31,28 @@
                   :title nil
                   :url (first lspl)) db)))
 
+(defn fetch-url
+  "Return the web page as a string."
+  [address]
+  (let [url (URL. address)]
+    (with-open [stream (. url (openStream))]
+      (let [buf (BufferedReader. (InputStreamReader. stream))]
+        (apply str (line-seq buf))))))
 
-           
+
+;;; getting ETag and Last-Modified
+(let [[status headers body] (http-get "http://ajaxian.com/index.xml")]
+        (list (get headers "ETag") (get headers "Last-Modified"))) 
+
+(defn check-feed
+  "Check if feed has been updated and grab it if it has."
+  [feed etag lmodif]
+  (let [[status headers body] (http-get feed { "If-Modified-Since" lmodif 
+                                               "If-None-Matc" etag })]
+    (println (str "Status: " status))
+    (doseq [h (keys headers)]
+      (println (str h " " (get headers h))))
+    (when (or (= status 200)
+              (= status 304))
+      (list (get headers "ETag") (get headers "Last-Modified")))))
+      
