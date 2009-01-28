545c7c8a1e2e625f39cc7bb29c8535f3efa61425
Joseph Fahey <joseph@feather.eerayell.net>
2009-01-28 10:20:35 +0100

    Rearranged order of functions so that file can load/compile.

--- clojrss.clj
+++ clojrss.clj
@@ -14,21 +14,22 @@
 
 ;; from http://groups.google.com/group/clojure/browse_thread/thread/380fdd164a5c5b7a/6c2ce06780ce5ddf?lnk=gst&q=line-seq&pli=1
 
-(defn parse-rsslist-file [db urlfile]
-  (with-open [r (reader urlfile)]
-    (parssrss  (line-seq r) db)))
-
-(defn parssrss [sq db]
-  (if (not sq)
-    db
-    (recur (rest sq) (db-add-line db (first sq)))))
-
 (defn db-add-line [db line]
   (let [lspl (.split line " ")]
     (cons (struct-map feed 
                   :name (second lspl)
                   :title nil
                   :url (first lspl)) db)))
+
+(defn parssrss [sq db]
+  (if (not sq)
+    db
+    (recur (rest sq) (db-add-line db (first sq)))))
+
+(defn parse-rsslist-file [db urlfile]
+  (with-open [r (reader urlfile)]
+    (parssrss  (line-seq r) db)))
+
 
 (defn db-save [db filename]
   (spit 
@@ -38,11 +39,6 @@
 (defn db-load [filename]
   (with-in-str  (slurp filename)
     (read)))
-
-
-;;; getting ETag and Last-Modified
-(let [[status headers body] (http-get "http://ajaxian.com/index.xml")]
-        (list (get headers "ETag") (get headers "Last-Modified"))) 
 
 (defn check-feed
   "Check if feed has been updated and grab it if it has. Returns a
@@ -65,6 +61,18 @@ vector [body status ETag Last-Modified"
       [ body status  
         (get headers "ETag") 
         (get headers "Last-Modified")])))
+
+(defn rss-filename [filename]
+  (let [dir "/home/joseph/localrss/dl/"
+        f (str filename)]
+   (if
+       (= (.indexOf f ".xml") -1)
+     (str dir f ".xml")
+     (str dir f))))
+
+
+(defn write-rss-file [filename data]
+  (spit (rss-filename filename) data))
       
 (defn feed-struct-update [feed]
   (let [feedvec (check-feed 
@@ -77,13 +85,24 @@ vector [body status ETag Last-Modified"
         :etag (nth feedvec 2)
         :lmodif (nth feedvec 3)))))
 
-(defn write-rss-file [filename data]
-  (spit (rss-filename filename) data))
 
-(defn rss-filename [filename]
-  (let [f (str filename)]
-   (if
-       (= (.indexOf f ".xml") -1)
-     (str f ".xml")
-     f)))
 
+
+(defn replace-feed-by-url [db url nfeed]
+  "Finds a feed in the db and replaces it with nfeed. Returns the new
+db."
+  (into (empty db)
+        (map 
+         (fn [feed]
+           (if (= (str (get feed :url))
+                  url)
+             nfeed
+             feed))
+         db)))
+             
+(defn update-feed [db feed]
+  "Checks feed and returns an updated version of db."
+  (replace-feed-by-url db
+                       (get feed :url)
+                       ;updating file is a side effect here
+                       (feed-struct-update feed))) 
